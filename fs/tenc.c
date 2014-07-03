77104c55df2caa8277cb12a50798cafa755c6fd5
Przemyslaw Horban <extremegf@gmail.com>
2014-07-03 17:31:45 +0200

    encryption code file

---
+++ fs/tenc.c
@@ -0,0 +1,10 @@
+/*
+ * Heart of transparent encryption system for ext4.
+ *
+ * Author: Przemyslaw Horban (p.horban@mimuw.edu.pl)
+ */
+
+
+int tenc_write_switch_page(struct page *page) {
+	return 0;
+}
