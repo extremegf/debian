ab74390a603585e6b5103549babdfed7b4a66f86
Przemyslaw Horban <extremegf@gmail.com>
2014-09-01 22:48:41 +0200

    linux side updates

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 4 */
 /* SMP */
 #define UTS_MACHINE "i386"
-#define UTS_VERSION "#1 SMP Mon Sep 1 14:23:00 CEST 2014"
+#define UTS_VERSION "#4 SMP Mon Sep 1 19:22:38 CEST 2014"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "zso2014"
 #define LINUX_COMPILER "gcc version 4.7.2 (Debian 4.7.2-5) "
