3f8c920c0c732f32aeb05f74d6baac546011dd3c
Przemyslaw Horban <extremegf@gmail.com>
2014-09-01 14:35:57 +0200

    added files usefull for code completion

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "i386"
+#define UTS_VERSION "#1 SMP Mon Sep 1 14:23:00 CEST 2014"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "zso2014"
+#define LINUX_COMPILER "gcc version 4.7.2 (Debian 4.7.2-5) "
