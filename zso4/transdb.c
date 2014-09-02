3d68dacc2d1623d4383b0879ad92a80306620a18
Przemyslaw Horban <extremegf@gmail.com>
2014-09-02 17:25:47 +0200

    Autocommit

--- zso4/transdb.c
+++ zso4/transdb.c
@@ -14,76 +14,77 @@
 #include <linux/module.h>
 
 static ssize_t hello_read(struct file * file, char * buf,
-                          size_t count, loff_t *ppos)
+		size_t count, loff_t *ppos)
 {
-        char *hello_str = "Hello, world!\n";
-        int len = strlen(hello_str); /* Don't include the null byte. */
-        /*
-         * We only support reading the whole string at once.
-         */
-        if (count < len)
-                return -EINVAL;
-        /*
-         * If file position is non-zero, then assume the string has
-         * been read and indicate there is no more data to be read.
-         */
-        if (*ppos != 0)
-                return 0;
-        /*
-         * Besides copying the string to the user provided buffer,
-         * this function also checks that the user has permission to
-         * write to the buffer, that it is mapped, etc.
-         */
-        if (copy_to_user(buf, hello_str, len))
-                return -EINVAL;
-        /*
-         * Tell the user how much data we wrote.
-         */
-        *ppos = len;
+	char *hello_str = "Hello, world!\n";
+	int len = strlen(hello_str); /* Don't include the null byte. */
+	/*
+	 * We only support reading the whole string at once.
+	 */
+	if (count < len)
+		return -EINVAL;
+	/*
+	 * If file position is non-zero, then assume the string has
+	 * been read and indicate there is no more data to be read.
+	 */
+	if (*ppos != 0)
+		return 0;
+	/*
+	 * Besides copying the string to the user provided buffer,
+	 * this function also checks that the user has permission to
+	 * write to the buffer, that it is mapped, etc.
+	 */
+	if (copy_to_user(buf, hello_str, len))
+		return -EINVAL;
+	/*
+	 * Tell the user how much data we wrote.
+	 */
+	*ppos = len;
 
-        return len;
+	return len;
 }
 
-static const struct file_operations hello_fops = {
-        .owner                = THIS_MODULE,
-        .read                = hello_read,
+static const struct file_operations db_fops = {
+		.owner                = THIS_MODULE,
+		.read                = hello_read,
 };
-static struct miscdevice hello_dev = {
-        /*
-         * We don't care what minor number we end up with, so tell the
-         * kernel to just pick one.
-         */
-        MISC_DYNAMIC_MINOR,
-        /*
-         * Name ourselves /dev/hello.
-         */
-        "hello",
-        /*
-         * What functions to call when a program performs file
-         * operations on the device.
-         */
-        &hello_fops
+
+static struct miscdevice db_device = {
+		/*
+		 * We don't care what minor number we end up with, so tell the
+		 * kernel to just pick one.
+		 */
+		MISC_DYNAMIC_MINOR,
+		/*
+		 * Name ourselves /dev/db.
+		 */
+		"db",
+		/*
+		 * What functions to call when a program performs file
+		 * operations on the device.
+		 */
+		&db_fops
 };
 
 static int transdb_init_module(void) {
-	 int ret;
+	int ret;
 
-	        /*
-	         * Create the "hello" device in the /sys/class/misc directory.
-	         * Udev will automatically create the /dev/hello device using
-	         * the default rules.
-	         */
-	        ret = misc_register(&hello_dev);
-	        if (ret)
-	                printk(KERN_ERR
-	                       "Unable to register \"Hello, world!\" misc device\n");
+	/*
+	 * Create the "db" device in the /sys/class/misc directory.
+	 * Udev will automatically create the /dev/db device using
+	 * the default rules.
+	 */
+	ret = misc_register(&db_dev);
+	if (ret)
+		printk(KERN_ERR
+				"Unable to register \"Hello, world!\" misc device\n");
 
-	        return ret;
+	return ret;
 	return 0;
 }
 
 static void transdb_cleanup_module(void) {
-    misc_deregister(&hello_dev);
+	misc_deregister(&db_dev);
 }
 
 module_init(transdb_init_module);
