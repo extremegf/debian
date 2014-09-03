c35583c19922524e70de78a29f2fe0e4c5541990
Przemyslaw Horban <extremegf@gmail.com>
2014-09-03 20:51:53 +0200

    Autocommit

--- zso4/main.c
+++ zso4/main.c
@@ -54,6 +54,8 @@ static ssize_t transdb_rw(rw_t rw, struct file *filp,
     size_t copied = 0;
     struct trans_context_t *trans = open_trans_if_needed(filp);
 
+    printk(KERN_INFO "We got a transdb_rw\n");
+
     if (!trans) {
         return -ENOMEM;
     }
