eb49f064fd1d1c18f90eac9c75642f53a6ddfcc9
Przemyslaw Horban <extremegf@gmail.com>
2014-09-03 20:45:57 +0200

    Autocommit

--- zso4/testy/test1/random_sync_writing.c
+++ zso4/testy/test1/random_sync_writing.c
@@ -5,7 +5,7 @@
 const size_t TEST_AREA_LENGTH = 10;
 const size_t TEST_COUNT = 10;
 const size_t MAX_SPAN = 2;
-const bool VERBOSE = false;
+const bool VERBOSE = 1;
 
 #define deb(...) if(VERBOSE) { printf (__VA_ARGS__); }
 
