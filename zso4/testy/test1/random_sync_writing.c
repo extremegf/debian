0e61714fd112ec92c1824683406bd0805a2f29d7
Przemyslaw Horban <extremegf@gmail.com>
2014-09-03 20:30:01 +0200

    Autocommit

--- zso4/testy/test1/random_sync_writing.c
+++ zso4/testy/test1/random_sync_writing.c
@@ -3,6 +3,8 @@
 #include <ctype.h>
 
 const size_t TEST_AREA_LENGTH = 100000;
+const size_t TEST_COUNT = 1000000;
+const size_t MAX_SPAN = 10000;
 
 #define CHECK(cond) \
 	if (!(cond)) { \
@@ -13,15 +15,59 @@ const size_t TEST_AREA_LENGTH = 100000;
 int main() {
 	FILE *db, *ref;
 
-	db = fopen("/dev/db", "r+");
+	db = fopen("/dev/db", "rb+");
 	CHECK(db != NULL);
 
-	ref = fopen("ref.txt", "w+");
+	ref = fopen("ref.txt", "wb+");
 	CHECK(ref != NULL);
 
 	// Clear out the ref file. Might be stale.
 	for (int i = 0; i < TEST_AREA_LENGTH; i++) {
 		fputc(0, ref);
+	}
+
+	for (size_t test_nr = 0; test_nr < TEST_COUNT; test_nr++) {
+		size_t pos, len;
+		char *data, *ref_data;
+
+		do {
+		   pos = random() % TEST_AREA_LENGTH;
+		   len = random() % MAX_SPAN;
+		} while (pos + len >= TEST_AREA_LENGTH);
+
+
+		if (rand() % 2 == 0) {
+			// Write!
+			data = new char[len];
+			for (size_t i = 0; i < len; i++) {
+				data[i] = rand() % 0x100;
+			}
+			fseek(db, pos, SEEK_SET);
+			fseek(ref, pos, SEEK_SET);
+
+			fwrite(data, 1, len, db);
+			fwrite(data, 1, len, ref);
+			delete[] data;
+		}
+		else {
+			// Read!
+			data = new char[len];
+			ref_data = new char[len];
+
+			fseek(db, pos, SEEK_SET);
+			fseek(ref, pos, SEEK_SET);
+
+			fread(data, 1, len, db);
+			fread(ref_data, 1, len, ref);
+
+			for (size_t i = 0; i < len; i++) {
+				CHECK(data[i] == ref_data[i]);
+			}
+
+			delete[] data;
+			delete[] ref_data;
+		}
+
 	}
 
 	puts("OK");
