1c232b23ceebd32cbef110e667f706691fafee57
Przemyslaw Horban <extremegf@gmail.com>
2014-09-03 11:20:01 +0200

    Autocommit

--- zso4/transaction.c
+++ zso4/transaction.c
@@ -7,9 +7,12 @@
 #include <linux/list.h>
 #include <linux/radix-tree.h>
 #include <linux/rwsem.h>
-
+#include <linux/slab.h>
+#include <linux/errno.h>
 
 #include "transaction.h"
+
+#define RADIX_TREE_GANG_SIZE 100
 
 typedef size_t ver_t;
 
@@ -23,7 +26,6 @@ struct db_seg {
     char data[SEGMENT_SIZE];
 };
 
-//    INIT_RADIX_TREE(my_tree, gfp_mask);
 struct db_version {
     struct radix_tree_root segments;
     struct db_version *parent;
@@ -53,6 +55,80 @@ ver_t next_ver;
 // Locks chain traversal, during compaction.
 struct rw_semaphore chain_rw_sem;
 
+static struct db_version *new_db_version(struct db_version *parent) {
+	struct db_version *ver = kmalloc(GFP_KERNEL, sizeof(struct db_version));
+	if (!ver)
+		return NULL;
 
+    INIT_RADIX_TREE(&ver->segments, GFP_KERNEL);
+    list_add(&ver->all_other, &all_db_vers);
+    ver->parent = parent;
+    return ver;
+}
 
+static void destroy_db_version(struct db_version *ver) {
+	size_t indices[RADIX_TREE_GANG_SIZE];
+	size_t found, start = 0, i;
+
+	do {
+		void **slot;
+		struct radix_tree_iter iter;
+		found = 0;
+
+		radix_tree_for_each_slot(slot, &ver->segments, iter, start)	{
+			indices[found] = iter->index;
+			found += 1;
+
+			if (found == RADIX_TREE_GANG_SIZE) {
+				break;
+			}
+		}
+
+		start = iter->index;
+
+		for (i = 0; i < found; i++) {
+			void *db_seg = radix_tree_delete(&ver->segments);
+			kfree(db_seg);
+		}
+	} while(found > 0);
+
+	list_del(&ver->all_other);
+
+	kfree(ver);
+}
+
+int trans_init() {
+	INIT_LIST_HEAD(&all_db_vers);
+
+	db_cur_ver = new_db_version(NULL);
+	if (!db_cur_ver)
+		return -ENOMEM;
+
+	commits_since_compat = 0;
+
+	memset(&null_seg->data, 0, SEGMENT_SIZE);
+	null_seg->ver_id = 0;
+
+	next_ver = 1;
+
+	init_rwsem(&chain_rw_sem);
+}
+
+void trans_destroy() {
+	struct db_version *ver, *n;
+
+	list_for_each_entry_safe(ver, n, &all_db_vers, all_other) {
+		destroy_db_version(ver);
+	}
+
+}
+
+/* Design
+
+trans_destroy()
+destroy po db_version
+zwalnia odbiekty db_version
+destroy locka?
+
+*/
 
