3ccd8b70462e5b467924234b2669bc2b89e3991a
Przemyslaw Horban <extremegf@gmail.com>
2014-09-04 11:10:42 +0200

    Autocommit

--- zso4/testy/test1/random_sync_writing.c
+++ zso4/testy/test1/random_sync_writing.c
@@ -6,7 +6,7 @@
 using namespace std;
 
 const size_t TEST_AREA_LENGTH = 10;
-const size_t TEST_COUNT = 100;
+const size_t TEST_COUNT = 20;
 const size_t MAX_SPAN = 2;
 const bool VERBOSE = 1;
 const bool ONLY_READS = 1;
