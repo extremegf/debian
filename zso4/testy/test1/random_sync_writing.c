4bc8e7c0504f908f16f35d14c9c43b1fbc9e8f24
Przemyslaw Horban <extremegf@gmail.com>
2014-09-04 11:46:00 +0200

    Autocommit

--- zso4/testy/test1/random_sync_writing.c
+++ zso4/testy/test1/random_sync_writing.c
@@ -15,7 +15,6 @@ const bool ONLY_READS = 0;
 
 #define deb(...) if(VERBOSE) { printf (__VA_ARGS__); }
 
-
 #define CHECK(cond) \
 	if (!(cond)) { \
 		fprintf(stderr, "%s:%d: CHECK(" #cond ") failed\n", __FILE__, __LINE__); \
@@ -39,7 +38,7 @@ int lseekarray(size_t pos, int _) {
 }
 
 size_t readarray(char *data, size_t len) {
-	for(size_t i = 0; i < len; i++) {
+	for (size_t i = 0; i < len; i++) {
 		data[i] = ARRAY[arr_pos];
 		arr_pos += 1;
 	}
@@ -47,7 +46,7 @@ size_t readarray(char *data, size_t len) {
 }
 
 size_t writearray(char *data, size_t len) {
-	for(size_t i = 0; i < len; i++) {
+	for (size_t i = 0; i < len; i++) {
 		ARRAY[arr_pos] = data[i];
 		arr_pos += 1;
 	}
@@ -67,32 +66,27 @@ int main() {
 		char *data, *ref_data;
 
 		do {
-		   pos = random() % TEST_AREA_LENGTH;
-		   len = 1 + random() % MAX_SPAN;
+			pos = random() % TEST_AREA_LENGTH;
+			len = 1 + random() % MAX_SPAN;
 		} while (pos + len > TEST_AREA_LENGTH);
 
+		CHECK_EQ(lseek(dbf, pos, SEEK_SET), 0);
+		CHECK_EQ(lseekarray(pos, SEEK_SET), 0);
+
+		data = new char[len];
+		ref_data = new char[len];
 
 		if (!ONLY_READS && rand() % 2 == 0) {
 			deb("Test %d read(count=%d, f_pos=%d)\n", test_nr, len, pos);
 
-			data = new char[len];
 			for (size_t i = 0; i < len; i++) {
 				data[i] = rand() % 0x100;
 			}
-			lseek(dbf, pos, SEEK_SET);
-			lseekarray(pos, SEEK_SET);
 
 			CHECK_EQ(writearray(data, len), len);
 			CHECK_EQ(write(dbf, data, len), len);
-			delete[] data;
-		}
-		else {
+		} else {
 			deb("Test %d write(count=%d, f_pos=%d)\n", test_nr, len, pos);
-			data = new char[len];
-			ref_data = new char[len];
-
-			lseek(dbf, pos, SEEK_SET);
-			lseekarray(pos, SEEK_SET);
 
 			CHECK_EQ(readarray(ref_data, len), len);
 			CHECK_EQ(read(dbf, data, len), len);
@@ -100,10 +94,10 @@ int main() {
 			for (size_t i = 0; i < len; i++) {
 				CHECK_EQ(data[i], ref_data[i]);
 			}
-
-			delete[] data;
-			delete[] ref_data;
 		}
+
+		delete[] data;
+		delete[] ref_data;
 	}
 
 	puts("OK");
